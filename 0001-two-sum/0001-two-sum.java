c54ecc222d2c97e44925b9c60c0df9376e94b4b5
Yashvi <yashvi.dev4@gmail.com>
2025-02-01 20:15:44 +0530

    0001-two-sum.java
    Two ways of solving the same problem , with different time  complexities

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -9,5 +9,25 @@ class Solution {
     }
     return new int[]{};
 }
+   // this is brute force approch tc--> o(n^2)
+   // but there is one more way that is hash map approach gives -> o(n) time complexity 
+//    import java. util.*;
+
+// public int[] twoSum(int[] nums, int target) {
+//     Map<Integer, Integer> map = new HashMap<>();  // Stores (value -> index)
+
+//     for (int i = 0; i < nums.length; i++) {
+//         int complement = target - nums[i];  // Calculate required complement
+
+//         if (map.containsKey(complement)) {  // If complement exists in map
+//             return new int[]{map.get(complement), i};  // Return stored index & current index
+//         }
+
+//         map.put(nums[i], i);  // Store current value and its index in the map
+//     }
+    
+//     return new int[]{};  // No solution found (should not happen in valid input)
+}
+
 
 }
