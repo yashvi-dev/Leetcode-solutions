286ed6169bcefef39a868e103f4e449364cb4dcd
Yashvi <yashvi.dev4@gmail.com>
2025-07-16 22:18:54 +0530

    Update 0011-container-with-most-water.cpp

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -21,6 +21,21 @@ public:
         int rp=height.size()-1;
 
         while(lp<rp){
+            /*🔹 Condition: while (lp < rp)
+➡️ Why do we loop while lp < rp?
+
+Because:
+
+You’re starting with the widest possible container: left end and right end.
+
+Then, you’re trying to narrow the width but possibly find taller lines to increase the area.
+
+Once lp == rp, it means both pointers are on the same line, and a container can't be formed — width would be zero.
+
+So, we loop as long as there are at least two lines to form a container.*/
+
+            // here width=right_index−left_index
+            // so we are getting the width from the index
             int width = rp-lp;
             int ht= min(height[lp], height[rp]);
             int area= width*ht;
